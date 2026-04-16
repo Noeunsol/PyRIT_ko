e4ae707b450af629e009d7f42ffb54ebc1131dcb
selectstar <selectstar@selectstarui-MacBookPro-5.local>
2026-04-16 18:42:01 +0900

    [DELETE] unit_tests, tutorials, doc

--- unit_tests/unit/memory/memory_interface/test_interface_core.py
+++
@@ -1,8 +0,0 @@
-# Copyright (c) Microsoft Corporation.
-# Licensed under the MIT license.
-
-from pyrit.memory import MemoryInterface
-
-
-def test_memory(sqlite_instance: MemoryInterface):
-    assert sqlite_instance
